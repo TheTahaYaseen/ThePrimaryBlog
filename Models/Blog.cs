c29e7552a413f98d231736e6fec3f6b49b79b67e
The Taha Yaseen <thetahayaseen@gmail.com>
2024-04-30 23:19:45 +0500

    Init commit

---
+++ Models/Blog.cs
@@ -0,0 +1,14 @@
+﻿namespace ThePrimaryBlog.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title  { get; set; }
+        public string Content { get; set; }
+
+        public Blog()
+        {
+
+        }
+    }
+}
